2b058f5ff8bf658fc22592e493b25b39dc645690
Juandomz91 <juandomz87@gmail.com>
2025-04-02 10:08:59 +0000

    empezamos

---
+++ app.js
@@ -0,0 +1,22 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let domain = ['.com', '.net', '.us', '.io'];
+
+const myDomain = [];
+
+function createMyDomain (arr1, arr2, arr3, arr4){
+    for(let i = 0; i < pronoun.length; i++){
+        myDomain.push(item);
+        for(let i = 0; i < adj.length; i++){
+            myDomain.push(item);
+            for(let i = 0; i = noun.length; i++){
+                myDomain.push(item);
+                for(let i = 0; i = domain.length; i++){
+                    myDomain.push(item);
+                }
+            }
+        }
+    }
+}
+console.log(createMyDomain);
